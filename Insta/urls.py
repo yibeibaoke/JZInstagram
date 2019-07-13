39cfce2faabaf4884ad44a1700f20bc58c3cdc15
WenTailai <wentailai.jiuzhang@gmail.com>
2019-07-13 18:24:27 -0400

    render static page

--- Insta/urls.py
+++ Insta/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from Insta.views import HelloDjango
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('Insta.urls')),
+    path('', HelloDjango.as_view(), name='home'),
 ]
---
+++ Insta/views.py
@@ -0,0 +1,6 @@
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+class HelloDjango(TemplateView):
+    template_name = 'home.html'
