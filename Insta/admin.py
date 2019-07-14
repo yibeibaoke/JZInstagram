c5a5d6f2b3f93e826191eb2c6c9261110a8156b3
WenTailai <wentailai.jiuzhang@gmail.com>
2019-07-14 19:32:12 -0400

    Add comments and likes

--- Insta/admin.py
+++ Insta/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from Insta.models import Post, InstaUser
+from Insta.models import InstaUser, Post, Comment, Like
 
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(InstaUser)
+admin.site.register(Comment)
+admin.site.register(Like)
--- Insta/models.py
+++ Insta/models.py
@@ -5,7 +5,23 @@ from imagekit.models import ProcessedImageField
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class InstaUser(AbstractUser):
+    profile_pic = ProcessedImageField(
+        upload_to = 'static/images/profiles',
+        format = 'JPEG',
+        blank = True,
+        null = True,
+    )
+
 class Post(models.Model):
+    author = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        blank = True,
+        null = True,
+    )
     title = models.TextField(blank=True, null=True)
     image = ProcessedImageField(
         upload_to = 'static/images/posts',
@@ -18,11 +34,42 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("post_detail", args=[str(self.id)])
-    
-class InstaUser(AbstractUser):
-    profile_pic = ProcessedImageField(
-        upload_to = 'static/images/profiles',
-        format = 'JPEG',
-        blank = True,
-        null = True,
+
+    def get_like_count(self):
+        return self.likes.count()
+
+    def get_comment_count(self):
+        return self.comments.count()
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments')
+    user = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='comments'
     )
+    comment = models.CharField(max_length=140)
+    posted_on = models.DateTimeField(
+        auto_now_add=True,
+        editable=False
+    )
+
+    def __str__(self):
+        return self.comment
+
+class Like(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='likes')
+    user = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='likes'
+    )
+
+    def __str__(self):
+        return 'Like: ' + self.user.username + ' Likes ' + self.post.title
